804b799db558fa882068b1f91114d1e7fc0f5cee
Spyros Mouselinos <mouselinos.spur.kw@gmail.com>
2024-03-03 21:16:52 +0100

    Solved

--- lab_1/min_k_solver.py
+++ lab_1/min_k_solver.py
@@ -1,5 +1,4 @@
 import sys
-
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import copy
@@ -17,7 +16,6 @@ for seq_record in SeqIO.parse("../data/yeast.fa", "fasta"):
     lengths.append(record_len)
 
 max_possible_k_value = min(lengths)
-print(max_possible_k_value)
 
 
 ### Are there any two sequences that are exactly the same?
@@ -32,8 +30,11 @@ def check_hash_match(strings):
 
 
 # Check for hash match
-#hash_match_found = check_hash_match(sequences)
-# print(hash_match_found)
+hash_match_found = check_hash_match(sequences)
+if hash_match_found:
+    print("There are at least 2 same sequences in the set")
+else:
+    print("All sequences are unique!")
 
 
 def kmers(s, k):
@@ -53,38 +54,86 @@ def kmers(s, k):
     return kmers
 
 
+# Solution Start #
+
+
 sequence_hashmap = {}
 kmer_hashmap = {}
 
-current_k = 464 # Correct Answer found after binary search over 2-700
-# For each new sequence
-for sid, sequence in enumerate(sequences):
-    sequence_hashmap[sid] = set()
-    # For k-mer in sequence
-    for kmer in kmers(sequence, current_k):
-        # Have we seen it so far?
-        if kmer in kmer_hashmap:
-            # If yes, go over the sequences we have seen it over and delete it from their sets.
-            for seen_sequence in kmer_hashmap[kmer]:
-                if seen_sequence != sid:
-                    # Go to sequence hashmap and delete it from the sequence
-                    sequence_hashmap[seen_sequence].remove(kmer)
-                    # Check if you left any sequence empty
-                    if len(sequence_hashmap[seen_sequence]) == 0:
-                        print("Not possible")
-                        sys.exit(1)
-                else:
-                    pass  # If we have seen it in the same sequence, do not do anything
-            # Remove it from the kmer_hashmap now so not to do double work
-            kmer_hashmap[kmer] = []
+
+
+
+def solve(sequences, current_k):
+    """
+    This function will act as the main logic to solve the problem. I make the following assumptions to make things
+    faster.
+    # Assumption 1: Since we are only interested in k, we do not need to find the complementary probes but just the
+        unique subsequences per sequence. Thus instead of performing .reverse_complement() to each sequence we can omit
+         it.
+    # Assumption 2: We can bound k in the range [1, min(len(sequence))], since any k larger than the shortest sequence
+        will not solve the problem. Furthermore, since we operate on unique sequences (we checked above), we can create
+         at least log_4(len(sequences)) without overlap ~ log_4(4437) = 6. So the final range is [6, 702]
+    # Assumption 3: While we can not make the solution faster, we can quickly find out when a current execution will not
+        lead to a correct result. It's very simple: If for any sequence, we have so far seen all k-mers that exist in
+        it,there is no point in executing more steps since there is at least 1 sequence that will not have a unique
+        probe. This is why we maintain a hashmap per sequence where we keep track of all the so far seen k-mers, if
+        we find that all the k-mers of any sequence have been seen, we terminate.
+    # Assumption 4: Apart from the sequence hashmap we can maintain another hashmap on the k-mers, to check if we have
+        already seen that k-mer. If yes, we go over all sequences where it exists and mark it as seen.
+    """
+
+    # For each new sequence
+    for sid, sequence in enumerate(sequences):
+        sequence_hashmap[sid] = set()
+        # For k-mer in sequence
+        for kmer in kmers(sequence, current_k):
+            # Have we seen it so far?
+            if kmer in kmer_hashmap:
+                # If yes, go over the sequences we have seen it over and delete it from their sets.
+                for seen_sequence in kmer_hashmap[kmer]:
+                    if seen_sequence != sid:
+                        # Go to sequence hashmap and delete it from the sequence
+                        sequence_hashmap[seen_sequence].remove(kmer)
+                        # Check if you left any sequence empty
+                        if len(sequence_hashmap[seen_sequence]) == 0:
+                            print("Not possible")
+                            return False
+                    else:
+                        pass  # If we have seen it in the same sequence, do not do anything
+                # Remove it from the kmer_hashmap now so not to do double work
+                kmer_hashmap[kmer] = []
+            else:
+                # If we have not seen it so far, update the two hashmaps.
+                kmer_hashmap[kmer] = [sid]
+                sequence_hashmap[sid].update([kmer])
+
+        # At the end, check if anything was added to the sequence_hashmap
+        if len(sequence_hashmap[sid]) == 0:
+            print("Not possible")
+            return False
+
+    print(f"Possible at {current_k}!")
+    return True
+
+
+search_range = [6, 702]
+
+
+def binary_search(low, high):
+    print(f"Checking k={high}\n")
+    if not solve(sequences, high):
+        return "No solution found within the given range."
+    while low < high:
+        mid = (low + high) // 2
+        print(f"Checking k={mid}\n")
+        if solve(sequences, mid):
+            high = mid
         else:
-            # If we have not seen it so far, update the two hashmaps.
-            kmer_hashmap[kmer] = [sid]
-            sequence_hashmap[sid].update([kmer])
+            low = mid + 1
+    return low
 
-    # At the end, check if anything was added to the sequence_hashmap
-    if len(sequence_hashmap[sid]) == 0:
-        print("Not possible")
-        sys.exit(1)
 
-print(f"Possible at {current_k}!")
+result = binary_search(6, 702)
+print(result)
+
+#k = 464 Correct Answer
